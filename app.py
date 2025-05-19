f157b416a0e0c4965b246ad3c845f2355bc5a192
kaua <goncalveskaua2k24@gmail.com>
2025-05-19 14:07:45 -0300

    funcionando

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import json
 import os
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
-
 DATA_FILE = 'habits.json'
 
 # Carrega os hábitos do arquivo JSON
@@ -17,6 +17,10 @@ def load_habits():
 def save_habits(habits):
     with open(DATA_FILE, 'w', encoding='utf-8') as f:
         json.dump(habits, f, indent=2)
+
+def get_last_days(n=7):
+    today = datetime.today()
+    return [(today - timedelta(days=1)).strftime('%Y-%m-%d') for i in reversed(range(n))] 
 
 # Página inicial
 @app.route('/')
@@ -33,6 +37,23 @@ def add():
         habits.append({"name": habit_name, "days": []})
         save_habits(habits)
     return redirect('/')
+
+@app.route('/toggle', methods=['POST'])
+def toggle():
+    habit_name = request.form['habit_name']
+    date = request.fprm['date']
+
+
+    habits = load_habits()
+    for habit in habits:
+        if habit['name'] == habit_name:
+            if date in habit['days']:
+                habit['days'].remove(date)
+            else:
+                habit['days'].append(date)
+                break
+            save_habits(habits)
+            return redirect('/')
 
 # Inicia o servidor Flask
 if __name__ == '__main__':
